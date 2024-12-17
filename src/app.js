0c4e04dce268a5fa3f1c84dbfabc0e88bfb26158
Armando7101 <164431619+Armando7101@users.noreply.github.com>
2024-12-17 00:28:10 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let quien = ["Una jirafa", "Mi abuela", "El taxista", "Mi sobrino de 3 meses"];
+let accion = ["se comió", "se orinó", "aplastó", "rompió"];
+let que = ["mi tarea", "mi laptop", "la moto"];
+let cuando = [
+  "en la madrugada",
+  "cuando estaba en el baño",
+  "mientras dormia",
+  "cuando estaba comiendo",
+  "cuando estaba haciendo voluntariado"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarExcusa() {
+  const quienIndex = Math.floor(Math.random() * quien.length);
+  const accionIndex = Math.floor(Math.random() * accion.length);
+  const queIndex = Math.floor(Math.random() * que.length);
+  const cuandoIndex = Math.floor(Math.random() * cuando.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return `${quien[quienIndex]} ${accion[accionIndex]} ${que[queIndex]} ${cuando[cuandoIndex]}.`;
+}
+function mostrarExcusa() {
+  const excuse = generarExcusa();
+  document.getElementById("excuse").innerHTML = excuse;
+}
+window.onload = mostrarExcusa;
